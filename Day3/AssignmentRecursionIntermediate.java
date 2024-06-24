335aa2b34945f55d82184455ba1c1773e930dd8e
zayar-myo-oo <zayarmyooo.dev@gmail.com>
2024-06-24 14:05:59 +0700

    java oop day3 exercises

--- Day3/AssignmentRecursionIntermediate.java
+++ Day3/AssignmentRecursionIntermediate.java
@@ -1,4 +1,52 @@
 package Day3;
 
 public class AssignmentRecursionIntermediate {
+    public static void main(String[] args) {
+        AssignmentRecursionIntermediate ass = new AssignmentRecursionIntermediate();
+        System.out.println(ass.power(2, 3));
+        System.out.println(ass.power(2, -3));
+        }
+
+    public double power(double base, int exponent) {
+        if (exponent == 0) {
+            return 1.0;
+        }
+        if (exponent < 0) {
+            return 1 / power(base, -exponent);
+        }
+        return base * power(base, exponent - 1);
+    }
+    public int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+    public int stringToInt(String s) {
+        if (s.isEmpty()) {
+            return 0;
+        } else {
+            int firstDigit = Character.getNumericValue(s.charAt(0));
+            return firstDigit * (int) Math.pow(10, s.length() - 1) + stringToInt(s.substring(1));
+        }
+    }
+    public void towerOfHanoi(int n, char fromRod, char toRod, char auxRod) {
+        if (n == 1) {
+            System.out.println("Move disk 1 from rod " + fromRod + " to rod " + toRod);
+            return;
+        }
+        towerOfHanoi(n - 1, fromRod, auxRod, toRod);
+        System.out.println("Move disk " + n + " from rod " + fromRod + " to rod " + toRod);
+        towerOfHanoi(n - 1, auxRod, toRod, fromRod);
+    }
+    public boolean isPalindrome(String s) {
+        if (s.length() == 0 || s.length() == 1) {
+            return true;
+        }
+        if (s.charAt(0) != s.charAt(s.length() - 1)) {
+            return false;
+        }
+        return isPalindrome(s.substring(1, s.length() - 1));
+    }
 }
